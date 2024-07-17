659bd0245d05b7258891bc4d14782c28e9ca0c89
Rayyan Aziz <itsRayan06@gmail.com>
2024-07-17 22:17:56 +0500

    Damage System Setup

--- Assets/Scripts/Controllers/PlayerController.cs
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -1,11 +1,20 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-partial class PlayerController : MonoBehaviour
+partial class PlayerController : MonoBehaviour, IDamageable
 {
+    [Header("Player")]
     public PlayerProfile profile;
     public PlayerCharacterProfile characterProfile;
 
     [Space]
+    [Header("Health")]
+    public int maxHealth = 100;
+    int currentHealth;
+    public int damage = 10; //DEBUGGING FOR NOW...
+
+    [Space]
+    [Header("References")]
     public Transform displayTransform;
     public Transform movementTransform;
 
@@ -25,20 +34,42 @@ partial class PlayerController : MonoBehaviour
 
     void Start()
     {
-        if(movementTransform == null)
+        if (movementTransform == null)
             movementTransform = transform;
 
         _rigidbody = GetComponent<Rigidbody>();
+        currentHealth = maxHealth;
     }
 
     void Update()
     {
         displayTransform.LookAt(transform.position + _look);
         displayTransform.rotation = Quaternion.Euler(0.0f, displayTransform.rotation.eulerAngles.y, 0.0f);
+
+        if (Keyboard.current.aKey.wasPressedThisFrame)
+        {
+            TakeDamage(damage);
+        }
     }
 
     void FixedUpdate()
     {
         _rigidbody.linearVelocity = _movement.normalized * (profile.speed * characterProfile.speed);
     }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+        if (currentHealth <= 0)
+        {
+            currentHealth = 0;
+            print("Player has died");
+        }
+        else
+        {
+            Debug.Log($"Player took {damage} damage, current health: {currentHealth}");
+        }
+    }
+
 }
