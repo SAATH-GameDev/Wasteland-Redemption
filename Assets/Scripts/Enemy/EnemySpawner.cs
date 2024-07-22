705c7413b619070831dfa5ee93e5f440a8a74d6c
Muhammad Khizar <khizarfarooq30@gmail.com>
2024-07-23 02:56:05 +0500

    Update EnemySpawner and EnemySpawnerProfile

--- Assets/Scripts/Enemy/EnemySpawner.cs
+++ Assets/Scripts/Enemy/EnemySpawner.cs
@@ -1,45 +1,77 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
     public EnemySpawnerProfile profile;
+    public Transform spawnTransform = null;
+    
+    private List<Enemy> enemies = new List<Enemy>();
     
     private float cooldown;
     
     private int limit;
     private int rate;
-
+    private int currentRemainingEnemies;
+    
+   
     private void Start()
     {
         cooldown = profile.cooldown;
         limit = profile.limit;
+        currentRemainingEnemies = limit;
         rate = profile.rate;
     }
 
-    private void Update()
-    {
-        cooldown -= Time.deltaTime;
-        
-        if(cooldown <= 0)
-        {
-            SpawnEnemy();
-            cooldown = profile.cooldown;
-        }
-    }
 
-    private void SpawnEnemy()
+    public void SpawnEnemy()
     {
         if (limit <= 0)
         {
             return;
         }
+        
+        Vector3 spawnPosition = Vector3.zero;
+        
+        switch (profile.spawnLocation)
+        {
+            case SpawnLocation.RANDOMPOSITIONINRADIUS:
+                spawnPosition = new Vector3(Random.Range(-profile.spawnRadius, profile.spawnRadius), 0, Random.Range(-profile.spawnRadius, profile.spawnRadius));
+                break;
+            case SpawnLocation.MAPBOUNDARY:
+                spawnPosition = new Vector3(Random.Range(profile.minMapBoundaryX.x, profile.minMapBoundaryX.y), 0, Random.Range(profile.minMapBoundaryZ.x, profile.minMapBoundaryZ.y));
+                break;
+            case SpawnLocation.CUSTOMTRANSFORM:
+                spawnPosition = spawnTransform !? spawnTransform.position : transform.position;
+                break;
+        }
 
         for (int i = 0; i < rate; i++)
         {
-            // todo #1 - replace spawn position to be determined by the profile with respect to player position
-            
-            Instantiate(profile.prefab, transform.position, Quaternion.identity);
+            Enemy newEnemy = Instantiate(profile.prefab, spawnPosition, Quaternion.identity);
+            enemies.Add(newEnemy);
             limit--;
         }
+    }
+    
+    public void DespawnEnemy(Enemy enemy)
+    {
+        currentRemainingEnemies--;
+        enemies.Remove(enemy);
+        Destroy(enemy.gameObject);
+    }
+    
+    public void KillAllEnemies()
+    {
+        foreach (Enemy enemy in enemies)
+        {
+            Destroy(enemy.gameObject);
+        }
+        enemies.Clear();
+    }
+    
+    public int GetActiveEnemiesCount()
+    {
+        return enemies.Count;
     }
 }
--- Assets/Scripts/ScriptableObjects/EnemySpawnerProfile.cs
+++ Assets/Scripts/ScriptableObjects/EnemySpawnerProfile.cs
@@ -3,5 +3,19 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Spawner Profile", menuName = "Profile/Enemy Spawner", order = 0)]
 public class EnemySpawnerProfile : SpawnerProfile<Enemy>
 {
-   
+    public SpawnLocation spawnLocation;
+    
+    [Space]
+    public float spawnRadius;
+    
+    [Space]
+    public Vector2 minMapBoundaryX;
+    public Vector2 minMapBoundaryZ;
+}
+
+public enum SpawnLocation
+{
+    RANDOMPOSITIONINRADIUS,
+    MAPBOUNDARY,
+    CUSTOMTRANSFORM
 }
