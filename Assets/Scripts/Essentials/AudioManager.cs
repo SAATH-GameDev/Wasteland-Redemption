7b4d912e924b263a638fa7dd8dc6db32256b84c0
bilalCh213 <programmercheema@gmail.com>
2024-08-13 23:11:18 +0500

    Added Audio Manager (WIP) and improved soundtrack for zombie wave attack (Start and Loop).

---
+++ Assets/Scripts/Essentials/AudioManager.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public float stopDuration = 3.0f;
+    [Space]
+    public AudioClip start;
+    public AudioClip loop;
+    [Space]
+    public bool played = false;
+    public bool stopping = false;
+
+    private AudioSource audioSource;
+
+    static public AudioManager Instance;
+
+    void Awake()
+    {
+        Instance = this;
+
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void Play(AudioClip start, AudioClip loop)
+    {
+        if(!start || !loop)
+            return;
+
+        if(!played && !stopping)
+        {
+            audioSource.PlayOneShot(start);
+
+            audioSource.clip = loop;
+            audioSource.loop = true;
+            audioSource.PlayDelayed(start.length);
+
+            played = true;
+        }
+    }
+
+    void Update()
+    {
+        Play(start, loop);
+
+        if(stopping)
+        {
+            audioSource.volume -= Time.unscaledDeltaTime / stopDuration;
+
+            if(audioSource.volume <= 0.0f)
+            {
+                played = false;
+                audioSource.Stop();
+                audioSource.volume = 1.0f;
+
+                audioSource.clip = start = loop = null;
+
+                stopping = false;
+            }
+        }
+    }
+}
